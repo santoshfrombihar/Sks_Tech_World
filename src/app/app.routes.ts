d84f79a6cdaebc497ccac546bb2b33dd2fe3d959
santoshfrombihar <santoshfrombihar8@gmail.com>
2025-01-19 11:47:14 +0530

    Login page completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { AuthFunctionComponent } from './src/app/auth-function/auth-function.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '' ,
+        component: AuthFunctionComponent
+    }
+];
---
+++ src/app/src/app/auth-function/auth-function.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-function',
+  imports: [],
+  templateUrl: './auth-function.component.html',
+  styleUrl: './auth-function.component.css'
+})
+export class AuthFunctionComponent {
+
+}
