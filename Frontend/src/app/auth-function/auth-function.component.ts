1edeacf99180ce28510be938335e58039bee43d3
santoshfrombihar <santoshfrombihar8@gmail.com>
2025-01-29 13:02:04 +0530

    Login Signup Page decorate

--- Frontend/src/app/auth-function/auth-function.component.ts
+++ Frontend/src/app/auth-function/auth-function.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-auth-function',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './auth-function.component.html',
   styleUrl: './auth-function.component.css'
 })
 export class AuthFunctionComponent {
+  isLogin: boolean = true; // Default: Registration mode
 
+  toggleLogin() {
+    this.isLogin = !this.isLogin;
+  }
 }
