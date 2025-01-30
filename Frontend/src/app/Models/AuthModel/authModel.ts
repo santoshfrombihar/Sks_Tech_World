6376ef61b97a5dbdf65baafaab7a484dadc17f4a
santoshfrombihar <santoshfrombihar8@gmail.com>
2025-01-30 12:29:37 +0530

    api integration to auth pages

---
+++ Frontend/src/app/Models/AuthModel/authModel.ts
@@ -0,0 +1,11 @@
+export interface UserLogin {
+    email: string;
+    password: string;
+}
+
+export interface UserRegistration {
+    firstName: string;
+    lastName: string;
+    email: string;
+    password: string;
+}
---
+++ Frontend/src/app/services/auth-service.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { UserRegistration } from '../Models/AuthModel/authModel';
+import { UserLogin } from '../Models/AuthModel/authModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthServiceService {
+
+  private apiUrl = 'https://localhost:7068/api/Auth'; 
+
+  constructor(private http: HttpClient) { }
+
+  // Register user
+  register(user: UserRegistration): Observable<UserRegistration> {
+    return this.http.post<UserRegistration>(`${this.apiUrl}/register`, user);
+  }
+
+  // Login user
+  login(user: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>(`${this.apiUrl}/login`, user);
+  }
+}
